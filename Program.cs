585b78ccaa6f9f6bfced0073addd86f43fa9aae1
Migs Morella <fhonkm@Migss-MacBook-Air.local>
2024-09-13 17:07:03 +0800

    DOTNET Practice Push

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HelloWorld;
+
+class Program
+{
+    static readonly int WindowWidth = Console.WindowWidth;
+    static readonly string[] ArrayTest = ["Test1", "Test2", "Test3"];
+    static void Main(string[] args)
+    {
+        Hello("Hello");
+
+        for (int i = 0; i < ArrayTest.Length; i++) {
+            Console.WriteLine(ArrayTest[i]);
+        }
+    }
+
+    public static void Hello(string message) {
+        Console.WriteLine($"{message} {WindowWidth}");
+    }
+}
